0f971db183a6e785270eb330bcfe89d70278b1f0
ASAngelica <asa196549@gmail.com>
2022-01-28 10:53:34 -0300

    Created Projet

---
+++ src/model/entities/Product.java
@@ -0,0 +1,49 @@
+package model.entities;
+
+public class Product {
+	
+	private String name;
+	private Double price;
+	private Integer quantity;
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	public double totalvalue() {
+		return price * quantity;
+	}
+	
+	@Override
+	public String toString() {
+		return name
+				+ ","
+				+ String.format("%.2f", this.totalvalue());
+	}
+}
